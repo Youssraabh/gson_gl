9e7435d9a2814a9956454561bfc3c694c4ba7a33
youssraabh <youssraabih@outlook.fr>
2024-04-01 16:18:10 +0200

    Renomme NonUploadingCaliperRunner en CaliperRunnerBenchmark

--- codemr/gson_gl 1/html/main_report/htmlx/modeldata/summary_data.js
+++
@@ -1,6 +0,0 @@
-var EQ_summaryInfo = [
-{label:"high coupling, high complexity, low cohesion", color: "#E50000", value: 530,count:1},
-{label:"high coupling, high complexity", color: "#FF5B13", value: 1517,count:3},
-{label:"high coupling", color: "#FFC800", value: 0,count:0},
-{label:"high complexity", color: "#62BF18", value: 5037,count:8},
-{label:"fair quality attributes", color: "#007F24", value: 19307,count:540}];
